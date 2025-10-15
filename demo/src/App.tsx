9f33fe69f95283c4249572be68a2a3566626bf94
nickrmurphy <117491495+nickrmurphy@users.noreply.github.com>
2025-10-14 19:00:15 -0500

    init todos

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,33 +1,47 @@
+import { useState } from "react";
 import { APITester } from "./APITester";
 import "./index.css";
 
-import logo from "./logo.svg";
-import reactLogo from "./react.svg";
+type Todo = {
+	text: string;
+	completed: boolean;
+};
 
 export function App() {
-	return (
-		<div className="max-w-7xl mx-auto p-8 text-center relative z-10">
-			<div className="flex justify-center items-center gap-8 mb-8">
-				<img
-					src={logo}
-					alt="Bun Logo"
-					className="h-24 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#646cffaa] scale-120"
-				/>
-				<img
-					src={reactLogo}
-					alt="React Logo"
-					className="h-24 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#61dafbaa] animate-[spin_20s_linear_infinite]"
-				/>
-			</div>
+	const [todos, setTodos] = useState<Todo[]>([]);
 
-			<h1 className="text-5xl font-bold my-4 leading-tight">Bun + React</h1>
-			<p>
-				<code className="bg-[#1a1a1a] px-2 py-1 rounded font-mono">
-					src/App.tsx
-				</code>{" "}
-				and save to test HMR
-			</p>
-			<APITester />
+	const [newTodo, setNewTodo] = useState("");
+
+	return (
+		<div className="space-y-6">
+			<h1 className="text-xl font-bold">Todos</h1>
+			<div className="flex gap-2">
+				<input
+					className="px-2 py-1 border border-white/10 rounded placeholder:text-white/50"
+					type="text"
+					placeholder="What needs to be done?"
+					value={newTodo}
+					onChange={(e) => setNewTodo(e.target.value)}
+				/>
+				<button
+					className="bg-indigo-900 rounded px-2 py-1 hover:bg-indigo-800 active:scale-105 disabled:opacity-50"
+					disabled={!newTodo.trim()}
+					onClick={() => {
+						setTodos([...todos, { text: newTodo, completed: false }]);
+						setNewTodo("");
+					}}
+				>
+					Add
+				</button>
+			</div>
+			<section className="divide-y divide-white/10">
+				{todos.map((todo, index) => (
+					<div key={index} className="flex items-center gap-2 p-2">
+						<input type="checkbox" className="w-4 h-4" />
+						<span className="flex-1">{todo.text}</span>
+					</div>
+				))}
+			</section>
 		</div>
 	);
 }
