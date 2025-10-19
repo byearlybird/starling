9a746a9a6b2c128d7598caef1abb22b0e5388089
nickrmurphy <117491495+nickrmurphy@users.noreply.github.com>
2025-10-19 10:09:09 -0500

    reactive query in solid

--- lib/solid/use-query.tsx
+++ lib/solid/use-query.tsx
@@ -1,22 +1,31 @@
 import { createQuery, type Store } from "@byearlybird/starling";
-import { createResource, onCleanup } from "solid-js";
+import { createEffect, createMemo, createResource, onCleanup } from "solid-js";
 
 export function useQuery<T extends object>(
 	store: Store<T>,
 	predicate: (data: T) => boolean,
 ) {
-	const query = createQuery(store, predicate);
-	const [data, { refetch, mutate }] = createResource(query.load, {
+	// Wrap in createMemo to track reactive dependencies in predicate
+	const query = createMemo(() => createQuery(store, predicate));
+
+	// Use query as source signal - refetches automatically when query changes
+	const [data, { mutate }] = createResource(query, (q) => q.load(), {
 		initialValue: {},
 	});
 
-	query.on("change", (data) => {
-		mutate(data);
-		refetch();
-	});
+	// Set up event listener that responds to data changes
+	createEffect(() => {
+		const currentQuery = query();
 
-	onCleanup(() => {
-		query.dispose();
+		const unsubscribe = currentQuery.on("change", (newData) => {
+			mutate(newData);
+		});
+
+		// Clean up listener when query changes or component unmounts
+		onCleanup(() => {
+			unsubscribe();
+			currentQuery.dispose();
+		});
 	});
 
 	return data;
