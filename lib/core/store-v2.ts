852340ae3c65560d6919a878263506aad2d34961
nickrmurphy <117491495+nickrmurphy@users.noreply.github.com>
2025-10-21 09:26:02 -0500

    batch insert

--- lib/core/store-v2.ts
+++ lib/core/store-v2.ts
@@ -12,8 +12,8 @@ type DeepPartial<T> = T extends object
 type Events<TValue> = {
     insert: { key: string; value: TValue }[];
     update: { key: string; value: TValue }[];
-    delete: { key: string }
-    mutate: undefined;
+    delete: { key: string }[];
+    mutate: { key: string }[];
 };
 
 export class Store<TValue extends object> {
@@ -29,7 +29,8 @@ export class Store<TValue extends object> {
     ) {
         this.#collectionKey = key;
         this.#storage = prefixStorage(storage, key);
-        this.#encode = (data: TValue | DeepPartial<TValue>) => encode(data, eventstampFn());
+        this.#encode = (data: TValue | DeepPartial<TValue>) =>
+            encode(data, eventstampFn());
     }
 
     get collectionKey() {
@@ -39,7 +40,32 @@ export class Store<TValue extends object> {
     async #mutate(key: string, value: EncodedObject) {
         await this.#storage.set(key, value);
         this.#cache.set(key, value);
-        this.#emitter.emit("mutate");
+        this.#emitter.emit("mutate", [{ key }]);
+    }
+
+    async #mutateAll(data: { key: string; value: EncodedObject }[]): Promise<{ success: boolean, mutatedKeys: string[] }> {
+        const mutatedKeys: string[] = [];
+
+        try {
+            const peristPromises = data.map(async (item) => {
+                await this.#storage.set(item.key, item.value);
+                this.#cache.set(item.key, item.value);
+                mutatedKeys.push(item.key);
+            })
+
+            await Promise.all(peristPromises);
+            return { success: true, mutatedKeys };
+
+        } catch (err: unknown) {
+            // todo enhance error handling / result types
+            console.error(err);
+            return { success: false, mutatedKeys }
+        }
+        finally {
+            if (mutatedKeys.length > 0) {
+                this.#emitter.emit('mutate', mutatedKeys.map((key) => ({ key })));
+            }
+        }
     }
 
     async init() {
@@ -59,15 +85,34 @@ export class Store<TValue extends object> {
         }
 
         const encoded = this.#encode(value);
-        await this.#mutate(key, encoded)
-            .then(() => {
-                this.#emitter.emit('insert', [{ key, value }]);
-            });
+        await this.#mutate(key, encoded).then(() => {
+            this.#emitter.emit("insert", [{ key, value }]);
+        });
+    }
+
+    async insertAll(data: { key: string; value: TValue }[]) {
+        const existingKeys = new Set(this.#cache.keys());
+        const newKeys = new Set(data.map((d) => d.key));
+        const conflictingKeys = existingKeys.intersection(newKeys);
+
+        if (conflictingKeys) {
+            const conflictingKeysString = Array.from(conflictingKeys)
+                .map((k) => k.toString())
+                .join(", ");
+            throw new Error(
+                `[${this.#collectionKey}]: Duplicate key(s): ${conflictingKeysString}`,
+            );
+        }
+
+        const encoded = data.map((d) => ({ key: d.key, value: this.#encode(d.value) }));
+        await this.#mutateAll(encoded);
+        this.#emitter.emit('insert', data);
     }
 
     async update(key: string, value: DeepPartial<TValue>) {
         const current = this.#cache.get(key);
-        if (!current) throw new Error(`[${this.#collectionKey}]: Key not found: ${key}`);
+        if (!current)
+            throw new Error(`[${this.#collectionKey}]: Key not found: ${key}`);
 
         const encoded = this.#encode(value);
         const [merged, changed] = merge(current, encoded);
@@ -75,21 +120,25 @@ export class Store<TValue extends object> {
         if (!changed) return;
 
         await this.#mutate(key, merged).then(() => {
-            this.#emitter.emit('update', [{ key, value: decode(merged) }]);
+            this.#emitter.emit("update", [{ key, value: decode(merged) }]);
         });
     }
 
     async delete(key: string) {
         const current = this.#cache.get(key);
-        if (!current) throw new Error(`[${this.#collectionKey}]: Key not found: ${key}`);
+        if (!current)
+            throw new Error(`[${this.#collectionKey}]: Key not found: ${key}`);
 
-        const [merged, changed] = merge(current, this.#encode({ __deleted: true } as TValue));
+        const [merged, changed] = merge(
+            current,
+            this.#encode({ __deleted: true } as TValue),
+        );
 
         if (!changed) return;
 
         await this.#mutate(key, merged).then(() => {
-            this.#emitter.emit('delete', { key });
-        })
+            this.#emitter.emit("delete", [{ key }]);
+        });
     }
 
     values(): Record<string, TValue> {
@@ -102,12 +151,15 @@ export class Store<TValue extends object> {
         return Object.fromEntries(this.#cache);
     }
 
-    on<K extends keyof Events<TValue>>(event: K, callback: (data: Events<TValue>[K]) => void) {
+    on<K extends keyof Events<TValue>>(
+        event: K,
+        callback: (data: Events<TValue>[K]) => void,
+    ) {
         this.#emitter.on(event, callback);
 
         return () => {
             this.#emitter.off(event, callback);
-        }
+        };
     }
 
     dispose() {
