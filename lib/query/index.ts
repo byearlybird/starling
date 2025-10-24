ede5f7f8e7900245c2263a85086b3b0b41cb752a
nickrmurphy <117491495+nickrmurphy@users.noreply.github.com>
2025-10-23 22:47:55 -0500

    x

--- lib/query/index.ts
+++
@@ -1,4 +0,0 @@
-export type { Query } from "./query";
-export { createQuery } from "./query";
-export type { Callback, Predicate, QueryEngine } from "./query-plugin";
-export { queryEngine } from "./query-plugin";
