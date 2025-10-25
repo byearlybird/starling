590e2148e5a7b78810e1dab8a39e222823caf1e9
nickrmurphy <117491495+nickrmurphy@users.noreply.github.com>
2025-10-25 14:16:49 -0500

    cleanup

--- packages/crdt/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { createClock, formatEventstamp, parseEventstamp } from "./clock";
-export { decode, encode, encodeMany, merge, mergeArray, decodeMany } from "./operations";
-export type { EncodedValue, EncodedObject, EventstampFn } from "./types";
