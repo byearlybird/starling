b8a91d4787fb038755f9a9e2626a8b81d642d499
nickrmurphy <117491495+nickrmurphy@users.noreply.github.com>
2025-10-26 01:32:59 -0500

    remove unused collection

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,4 @@
 import * as $clock from "./clock";
-import * as $collection from "./collection";
 import * as $document from "./document";
 import * as $eventsamp from "./eventstamp";
 import * as $map from "./map";
@@ -7,13 +6,4 @@ import * as $record from "./record";
 import * as $store from "./store";
 import * as $value from "./value";
 
-export {
-	$clock,
-	$collection,
-	$document,
-	$eventsamp,
-	$map,
-	$record,
-	$store,
-	$value,
-};
+export { $clock, $document, $eventsamp, $map, $record, $store, $value };
